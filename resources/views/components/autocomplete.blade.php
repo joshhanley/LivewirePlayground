be704ffadaac37814a9be1255518b0b662913f73
Josh Hanley <josh@hitsystems.com.au>
2020-10-29 10:17:44 +1100

    Add a loading overlay

--- resources/views/components/autocomplete.blade.php
+++ resources/views/components/autocomplete.blade.php
@@ -32,19 +32,28 @@
         class="w-full p-2 overflow-y-hidden text-sm rounded border border-cool-gray-300 bg-cool-gray-50 shadow-inner lg:absolute lg:p-0 lg:overflow-y-auto lg:max-h-96  lg:left-2 lg:mt-2 lg:w-56 lg:z-20 lg:border-cool-gray-400 lg:bg-white lg:shadow-lg"
         x-cloak
     >
-        <div class="overflow-y-auto max-h-96 border border-cool-gray-300 rounded shadow-sm bg-white lg:overflow-y-visible lg:max-h-none lg:border-0 lg:rounded-none lg:bg-transparent lg:shadow-none">
-            @foreach($results as $key => $result)
-                <div
-                    :class="{ 'bg-blue-500' : focusIndex == {{ $key }}}"
-                    class="px-2"
-                    wire:key="{{ $key }}"
-                    x-on:mouseenter="focusIndex = {{ $key }}"
-                    x-on:mouseenter="focusIndex = null"
-                    x-on:click="selectItem($dispatch)"
-                >
-                    <x-dynamic-component :component="$resultComponent" :model="$result" />
-                </div>
-            @endforeach
+        <div class="overflow-y-auto relative max-h-96 border border-cool-gray-300 rounded shadow-sm bg-white lg:overflow-y-visible lg:max-h-none lg:border-0 lg:rounded-none lg:bg-transparent lg:shadow-none">
+            <div wire:loading.class.remove="hidden" class="hidden absolute inset-0 flex items-center justify-center">
+                <div class="absolute inset-0 bg-gray-500 opacity-25"></div>
+                <svg class="animate-spin h-4 w-4 text-cool-gray-700 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </div>
+            <div>
+                @foreach($results as $key => $result)
+                    <div
+                        :class="{ 'bg-blue-500' : focusIndex == {{ $key }}}"
+                        class="px-2"
+                        wire:key="{{ $key }}"
+                        x-on:mouseenter="focusIndex = {{ $key }}"
+                        x-on:mouseenter="focusIndex = null"
+                        x-on:click="selectItem($dispatch)"
+                    >
+                        <x-dynamic-component :component="$resultComponent" :model="$result" />
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
