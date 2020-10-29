8318ba7d8473dfdb9153f5af2d0b36382e40ee67
Josh Hanley <josh@hitsystems.com.au>
2020-10-29 13:20:41 +1100

    Add second input as test

--- app/Http/Livewire/Main.php
+++ app/Http/Livewire/Main.php
@@ -7,8 +7,9 @@ use Livewire\Component;
 class Main extends Component
 {
     public $inputValue;
-    public $showDropdown = false;
+    public $inputValue2;
     public $selectedItem;
+    public $selectedItem2;
 
     public $results = [
         ['id' => 1, 'value' =>'aa'],
@@ -23,7 +24,27 @@ class Main extends Component
         ['id' => 10, 'value' =>'aaaaaaaaaaa']
     ];
 
+    public $results2 = [
+        ['id' => 1, 'value' =>'bb'],
+        ['id' => 2, 'value' =>'bbb'],
+        ['id' => 3, 'value' =>'bbbb'],
+        ['id' => 4, 'value' =>'bbbbb'],
+        ['id' => 5, 'value' =>'bbbbbb'],
+        ['id' => 6, 'value' =>'bbbbbbb'],
+        ['id' => 7, 'value' =>'bbbbbbbb'],
+        ['id' => 8, 'value' =>'bbbbbbbbb'],
+        ['id' => 9, 'value' =>'bbbbbbbbbb'],
+        ['id' => 10, 'value' =>'bbbbbbbbbbb']
+    ];
+
     public $filteredResults = [];
+    public $filteredResults2 = [];
+
+    public function mount()
+    {
+        $this->limitResults();
+        $this->limitResults2();
+    }
 
     public function selectIndex($data)
     {
@@ -37,11 +58,6 @@ class Main extends Component
             $this->limitResults();
             $this->dispatchBrowserEvent('input-change', ['value' => $this->inputValue]);
         }
-    }
-
-    public function mount()
-    {
-        $this->limitResults();
     }
 
     public function limitResults()
@@ -60,6 +76,38 @@ class Main extends Component
     public function updatedInputValue()
     {
         $this->limitResults();
+    }
+
+    public function selectIndex2($data)
+    {
+        if (isset($data['index'])) {
+            $index = $data['index'];
+
+            $this->selectedItem2 = $this->filteredResults2[$index] ?? null;
+
+            $this->inputValue2 = $this->selectedItem2['value'];
+
+            $this->limitResults2();
+            $this->dispatchBrowserEvent('input-change2', ['value' => $this->inputValue2]);
+        }
+    }
+
+    public function limitResults2()
+    {
+        if (is_null($this->inputValue2) || $this->inputValue2 == '') {
+            $this->filteredResults2 = $this->results2;
+        } else {
+            $this->filteredResults2 = array_values(array_filter($this->results2, function ($resultValue) {
+                return strpos($resultValue['value'], $this->inputValue2) !== false;
+            }));
+        }
+
+        $this->dispatchBrowserEvent('results-changed2', ['count' => count($this->filteredResults2)]);
+    }
+
+    public function updatedInputValue2()
+    {
+        $this->limitResults2();
     }
 
     public function render()
--- resources/views/livewire/main.blade.php
+++ resources/views/livewire/main.blade.php
@@ -1,22 +1,50 @@
 <div class="relative w-full h-screen p-4 flex flex-col  items-center bg-gray-100">
     <div class="flex flex-col space-y-4">
 
-        <div>
-            Selected Item:<span>{{ $this->selectedItem['value'] ?? null }}</span>
+        <div class="flex flex-col space-y-4" wire:key="1">
+            <div>
+                Selected Item:<span>{{ $this->selectedItem['value'] ?? null }}</span>
+            </div>
+            <div>
+                Input Value:<span>{{ $this->inputValue ?? null }}</span>
+            </div>
+            <x-autocomplete
+                wire:model.debounce.300ms="inputValue"
+                wire:select-item="selectIndex"
+                result-component="list-item"
+                :results="$this->filteredResults"
+                input-changed-event="input-change"
+                results-changed-event="results-changed"
+                item-selected-event="select-item"
+                inline
+                wire:key="1"
+            />
         </div>
-        <div>
-            Input Value:<span>{{ $this->inputValue ?? null }}</span>
+
+        <div class="flex flex-col space-y-4" wire:key="2">
+            <div>
+                Selected Item:<span>{{ $this->selectedItem2['value'] ?? null }}</span>
+            </div>
+            <div>
+                Input Value:<span>{{ $this->inputValue2 ?? null }}</span>
+            </div>
+            <x-autocomplete
+                wire:model.debounce.300ms="inputValue2"
+                wire:select-item2="selectIndex2"
+                result-component="list-item"
+                :results="$this->filteredResults2"
+                input-changed-event="input-change2"
+                results-changed-event="results-changed2"
+                item-selected-event="select-item2"
+                inline
+                wire:key="2"
+            />
+            <input class="border border-gray-500" type="text" />
         </div>
-        <x-autocomplete
-            wire:model.debounce.300ms="inputValue"
-            wire:select-item="selectIndex"
-            result-component="list-item"
-            :results="$this->filteredResults"
-            input-changed-event="input-change"
-            results-changed-event="results-changed"
-            item-selected-event="select-item"
-            inline
-        />
-        <input class="border border-gray-500" type="text" />
+
+        <div>
+            {{ count($this->filteredResults) }}
+            {{ count($this->filteredResults2) }}
+        </div>
     </div>
 </div>
