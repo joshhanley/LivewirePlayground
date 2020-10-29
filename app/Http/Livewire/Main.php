4f5d6b801c22bc8f60932c52d94c0be1138b9684
Josh Hanley <josh@hitsystems.com.au>
2020-10-29 17:25:29 +1100

    Clean up user functions

--- app/Http/Livewire/Main.php
+++ app/Http/Livewire/Main.php
@@ -22,32 +22,51 @@ class Main extends Component
     {
         $this->allUsers = User::all();
         $this->user = new User();
-        $this->user->name = "test";
         $this->userInput = $this->user->name;
+        $this->getUsers();
 
 
         $this->user2 = new User();
-        $this->user2->name = "test2";
         $this->userInput2 = $this->user2->name;
+        $this->getUsers2();
+    }
+
+    public function getUsers()
+    {
+        $this->users = User::where('name', 'LIKE', "%{$this->userInput}%")->get();
+    }
+
+    public function getUsers2()
+    {
+        $this->users2 = User::where('name', 'LIKE', "%{$this->userInput2}%")->get();
     }
 
     public function selectUser($focusIndex)
     {
         $this->user = $this->users[$focusIndex] ?? new User();
         $this->userInput = $this->user->name;
+        $this->getUsers();
     }
 
     public function selectUser2($focusIndex)
     {
         $this->user2 = $this->users2[$focusIndex] ?? new User();
         $this->userInput2 = $this->user2->name;
+        $this->getUsers2();
+    }
+
+    public function updatedUserInput()
+    {
+        $this->getUsers();
+    }
+
+    public function updatedUserInput2()
+    {
+        $this->getUsers2();
     }
 
     public function render()
     {
-        $this->users = User::where('name', 'LIKE', "%{$this->userInput}%")->get();
-        $this->users2 = User::where('name', 'LIKE', "%{$this->userInput2}%")->get();
-
         return view('livewire.main');
     }
 }
