1792a911a6bcfb022883a60cbf9e73bf1567ecb9
Josh Hanley <josh@hitsystems.com.au>
2020-10-29 12:30:00 +1100

    Add x-cloak style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,11 @@
 
         <livewire:styles />
         <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <style>
+            [x-cloak] {
+                display: none;
+            }
+        </style>
         @stack('styles')
 
 
