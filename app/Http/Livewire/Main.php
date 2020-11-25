6d51672aa1e5c098b1aa4302da2040e7e38cfbf9
Josh Hanley <josh@hitsystems.com.au>
2020-11-26 10:52:16 +1100

    Add test code

--- app/Http/Livewire/Main.php
+++ app/Http/Livewire/Main.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class Main extends Component
 {
+    public $no_entangle;
+    public $with_entangle;
+
     public function render()
     {
         return view('livewire.main');
--- resources/views/livewire/main.blade.php
+++ resources/views/livewire/main.blade.php
@@ -1,7 +1,54 @@
 <div class="relative w-full h-screen p-4 flex flex-col  items-center bg-gray-100">
     <div class="flex space-x-4">
         {{-- Put livewire component blade content here or reference another livewire component --}}
-        App
+        <div>
+            <form>
+
+                {{-- example of custom checkbox using Livewire V1.x method (no @entangle) --}}
+                <div
+                    wire:model="no_entangle"
+                    x-data="{ checked: $wire.get('no_entangle') }"
+                    @click="
+                        checked = ! checked;
+                        $dispatch('input', checked);
+                    "
+                    class="flex items-center h-6"
+                >
+                    <input
+                        type="checkbox"
+                        x-bind="checked"
+                        class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                    />
+                    <div class="ml-3 text-sm font-medium text-gray-700">
+                            No Entangle
+                            (
+                                Livewire value =  {{ $no_entangle ? 'true' : 'false' }},
+                                Alpine value =  <span x-text="checked ? 'true' : 'false'"></span>
+                            )
+                    </div>
+                </div>
+
+                {{-- example of custom checkbox using Livewire V2.x method (*with* @entangle) --}}
+                <div
+                    x-data="{ checked: @entangle('with_entangle') }"
+                    @click="checked = ! checked"
+                    class="flex items-center h-6"
+                >
+                    <input
+                        type="checkbox"
+                        x-bind="checked"
+                        class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                    />
+                    <div class="ml-3 text-sm font-medium text-gray-700">
+                            With Entangle
+                            (
+                                Livewire value =  {{ $with_entangle ? 'true' : 'false' }},
+                                Alpine value =  <span x-text="checked ? 'true' : 'false'"></span>
+                            )
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
@@ -13,6 +60,6 @@
 
     @push('scripts')
         {{-- Put any custom scripts or library scripts needed for this component here --}}
-        
+
     @endpush
 @endonce
