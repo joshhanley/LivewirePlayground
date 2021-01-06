d052a5f308b523f9ffa13c107874d6b1e2923fd6
Josh Hanley <josh@hitsystems.com.au>
2021-01-07 06:37:40 +1100

    Add authorize to mount method (make sure app debug is false)

--- app/Http/Livewire/Main.php
+++ app/Http/Livewire/Main.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
 
 class Main extends Component
 {
+    use AuthorizesRequests;
+
+    public function mount()
+    {
+        $this->authorize('can-do-a-thing');
+    }
+
     public function render()
     {
         return view('livewire.main');
