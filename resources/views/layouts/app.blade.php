a8a4be9593bcdd196e7774d193ecc4b334105d5c
Josh Hanley <josh@hitsystems.com.au>
2021-03-24 16:08:57 +1100

    Update Alpine to 2.8.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     </script>
     @endenv
 
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
 </head>
 
 <body class="antialiased">
