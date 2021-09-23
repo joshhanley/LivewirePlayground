d1aaec90d98ffcd91a9d4ef0ddd0aad2dd5b7a5b
Josh Hanley <josh@hitsystems.com.au>
2021-09-24 08:54:12 +1000

    Clean up render

--- app/Http/Livewire/Main.php
+++ app/Http/Livewire/Main.php
@@ -8,7 +8,6 @@ class Main extends Component
 {
     public function render()
     {
-        return view('livewire.main')
-            ->layout('layouts.app', ['title' => 'Main component title']);
+        return view('livewire.main');
     }
 }
