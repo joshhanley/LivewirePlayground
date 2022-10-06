28aa7ae81424f1d1ae7bfe51973b90f97b192b93
Josh Hanley <josh@hitsystems.com.au>
2022-10-06 21:14:20 +1100

    Add setup method to testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use Glhd\Dawn\Browser;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Route;
+use Livewire\Macros\DuskBrowserMacros;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Browser::mixin(new DuskBrowserMacros);
+
+        Route::get('/livewire-dusk/{component}', function ($component) {
+            $class = urldecode($component);
+
+        return app()->call(new $class());
+        })->middleware('web');
+    }
 }
