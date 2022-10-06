af715b8fbffe7b07cf865ebdf320fc6ca63ff04b
Josh Hanley <josh@hitsystems.com.au>
2022-10-06 21:14:59 +1100

    Clean up test

--- tests/Feature/SampleTest.php
+++ tests/Feature/SampleTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use App\Http\Livewire\Main;
 use Glhd\Dawn\RunsBrowserTests;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class SampleTest extends TestCase
@@ -15,18 +15,13 @@ class SampleTest extends TestCase
 
     public function testExample()
     {
-        Route::get('/livewire2-dusk/{component}', function ($component) {
-            $class = urldecode($component);
-
-            return app()->call(new $class());
-        })->middleware('web');
-        
         $class = Main::class;
         $queryString = '';
-        $url = '/livewire2-dusk/'.urlencode($class).$queryString;
+        $url = '/livewire-dusk/'.urlencode($class).$queryString;
 
         $this->openBrowser()
             ->visit($url)
+            ->waitForLivewireToLoad()
             ->assertSeeIn('body', 'App')
             ;
     }
